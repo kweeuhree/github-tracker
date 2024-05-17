9aac422a003effe6be8d3dd7fc9043cd77ba77fd
Nika K <firstnamenika@gmail.com>
2024-05-17 07:59:20 -0400

    routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/NavBar/Footer';
+import MainLayout from './pages/MainLayout';
+import UserSearch from './components/UserSearch/UserSearch';
 import viteLogo from '/vite.svg';
 import './App.css';
 
@@ -10,11 +12,15 @@ function App() {
   return (
     <div className="App">
       <NavBar />
-      <Routes>
-        <Router>
 
-        </Router>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route path='/user-search' element={<UserSearch />} />
+          <Route path='/kweeuhree' element={<UserSearch user='kweeuhree'/>} />
+          <Route path='/firstnamenika' element={<UserSearch user='firstnamenika'/>} />
+        </Route>
       </Routes>
+
       <Footer />
     </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import App from './App.jsx';
 import './index.css';
 
 
+import { BrowserRouter as Router } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
