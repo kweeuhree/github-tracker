35c331593b04401ba624aa00645d20ea3e556884
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-14 10:01:02 -0400

    experimental text animation in MainLayout

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -7,7 +7,12 @@ const MainLayout = ({ user }) => {
   return (
     <div className='main-layout'>
 
-        <div className='info'>github is amazing</div>
+        <div className='header'>
+          <div className='animate-text'>github is amazing</div>
+          <div className='animate-text2'>github is amazing</div>
+          <div className='animate-text3'>github is amazing</div>
+          <div className='image-overlay' />
+        </div>
         {/* <Button action={handleClick} type={'Find a User'} /> */}
         <UserSearch user={user}/>
     </div>
