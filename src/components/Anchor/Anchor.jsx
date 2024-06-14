0460d3052a4f5bc109f298d0a7c722a80862d9d5
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-14 13:31:43 -0400

    comments for Anchor

--- src/components/Anchor/Anchor.jsx
+++ src/components/Anchor/Anchor.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+// import external link icon
 import { BiLinkExternal } from "react-icons/bi";
+// css
 import './AnchorStyles.css';
 
 const Anchor = ({ href, innerHtml }) => {
   return (
+    // anchor
     <a href={href} target='_blank'>
+        {/* text of external link */}
         {innerHtml}
+        {/* space between text and external link icon */}
          &nbsp;
+        {/* external link icon */}
         <span>
             <BiLinkExternal />
         </span>
+
      </a>
   )
 }
