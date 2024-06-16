a4874c99fff511d85eaf1263dff2c58e359f5fab
Nika Kolesnikova <firstnamenika@gmail.com>
2024-06-16 09:04:47 -0400

    randomNotableUser is being passed form MainLayout

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,8 @@ import Footer from './components/NavBar/Footer';
 import MainLayout from './pages/MainLayout';
 import Header from './components/Header/Header';
 import UserSearch from './components/UserSearch/UserSearch';
-//notable users data
-import { getRandomNotableUser } from './utils/notableUsers';
 
 function App() {
-
-  const randomNotableUser = getRandomNotableUser();
-
 
   return (
     <main className="display-flex flex-col main-layout">
@@ -24,7 +19,7 @@ function App() {
       
       <Routes>
           <Route path='/' element={<Navigate to={'/search'}/>} />
-          <Route path='/search' element={<MainLayout user={randomNotableUser}/>} />
+          <Route path='/search' element={<MainLayout />} />
           <Route path='/kweeuhree' element={<UserSearch user={{nickname: 'kweeuhree'}} />} />
           <Route path='/firstnamenika' element={<UserSearch user={{nickname: 'firstnamenika'}} />} />
       </Routes>
